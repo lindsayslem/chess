66bd28c7194ac1241ef4c045d144f58e86deba6b
created squares for the chess board. implemented getRow and getColumn <lindsayslem@gmail.com>
2024-05-04 11:30:10 -0600

    implemented knight moves

---
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -0,0 +1,45 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class KnightMovesCalculator {
+    private final ChessGame.TeamColor pieceColor;
+    private final ChessBoard board;
+
+    public KnightMovesCalculator(ChessGame.TeamColor pieceColor, ChessBoard board) {
+        this.pieceColor = pieceColor;
+        this.board = board;
+    }
+
+    public Collection<ChessMove> pieceMove(ChessPosition position) {
+        List<ChessMove> legalMoves = new ArrayList<>();
+
+        int[][] moves = {{1, 2}, {2, 1}, {-1, 2}, {-2, -1}, {-2, 1}, {2, -1}, {1, -2}, {2, -1}};
+
+        for (int[] move : moves) {
+            int rowDirection = move[0];
+            int colDirection = move[1];
+
+            int currentRow = position.getRow() + rowDirection;
+            int currentCol = position.getColumn() + colDirection;
+
+            if (currentRow >= 0 && currentRow < 8 && currentCol >= 0 && currentCol < 8) {
+                ChessPosition newPosition = new ChessPosition(currentRow, currentCol);
+                ChessPiece newPositionPiece = board.getPiece(newPosition);
+
+
+                if (newPositionPiece == null) {
+                    legalMoves.add(new ChessMove(position, newPosition, null));
+                } else {
+                    if (newPositionPiece.getTeamColor() != pieceColor) {
+                        legalMoves.add(new ChessMove(position, newPosition, null));
+                    }
+                }
+            }
+        }
+        return legalMoves;
+    }
+}
+
