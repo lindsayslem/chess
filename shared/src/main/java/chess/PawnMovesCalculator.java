2d5c8d53b63acd9f808cfeda09507888a216ef06
created squares for the chess board. implemented getRow and getColumn <lindsayslem@gmail.com>
2024-05-05 22:07:03 -0600

    started pawn--still needs work

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -15,5 +15,28 @@ public class PawnMovesCalculator {
     public Collection<ChessMove> pieceMove(ChessPosition position) {
         List<ChessMove> legalMoves = new ArrayList<>();
 
+        int currentRow = position.getRow();
+        int currentCol = position.getColumn();
+
+        if(currentRow == 2 || currentRow == 6){
+            ChessPosition newPosition = new ChessPosition(currentRow + 2, currentCol);
+            legalMoves.add(new ChessMove(position, newPosition, null));
+        }
+        else{
+            ChessPosition forwardPosition = new ChessPosition(currentRow + 1, currentCol);
+            ChessPiece newPositionPiece = board.getPiece(newPosition);
+            if (forwardPositionPiece == null) {
+                legalMoves.add(new ChessMove(position, newPosition, null));
+            } else {
+                if (newPositionPiece.getTeamColor() != pieceColor) {
+                    legalMoves.add(new ChessMove(position, newPosition, null));
+                }
+            ChessPosition rightPosition = new ChessPosition(currentRow, currentCol + 1);
+            legalMoves.add(new ChessMove(position, rightPosition, null));
+            ChessPosition leftPosition = new ChessPosition(currentRow, currentCol -1);
+            legalMoves.add(new ChessMove(position, leftPosition, null));
+        }
+
+        return legalMoves;
     }
 }
