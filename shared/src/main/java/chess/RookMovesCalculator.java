53d07a6fee6c30effab387750166235ddc1760b0
created squares for the chess board. implemented getRow and getColumn <lindsayslem@gmail.com>
2024-06-04 17:11:49 -0600

    fixed duplicates bishop and rook

--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -12,37 +12,40 @@ public class RookMovesCalculator {
         this.board = board;
     }
 
-    public Collection<ChessMove> pieceMove(ChessPosition position){
+    public Collection<ChessMove> pieceMove(ChessPosition position) {
         List<ChessMove> legalMoves = new ArrayList<>();
 
         int[][] moves = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
 
-        for(int[] move : moves){
-            int rowDirection = move[0];
-            int colDirection = move[1];
-
-            int currentRow = position.getRow() + rowDirection;
-            int currentCol = position.getColumn() + colDirection;
-
-            while(currentRow > 0 && currentRow  < 9 && currentCol > 0 && currentCol < 9){
-                ChessPosition newPosition = new ChessPosition(currentRow, currentCol);
-                ChessPiece newPositionPiece = board.getPiece(newPosition);
-
-                if(newPositionPiece == null){
-                    legalMoves.add(new ChessMove(position, newPosition, null));
-                }
-                else{
-                    if(newPositionPiece.getTeamColor() != pieceColor){
-                        legalMoves.add(new ChessMove(position, newPosition, null));
-                    }
-                    break;
-                }
-                currentRow += rowDirection;
-                currentCol += colDirection;
-            }
-        }
-        /*returns all valid move*/
-        return legalMoves;
-
+        return rookAndBishopMovements(position, legalMoves, moves, board, pieceColor);
     }
-}
+
+        static Collection<ChessMove> rookAndBishopMovements(ChessPosition position, List<ChessMove> legalMoves, int[][] moves, ChessBoard board, ChessGame.TeamColor pieceColor)
+        {
+            for (int[] move : moves) {
+                int rowDirection = move[0];
+                int colDirection = move[1];
+
+                int currentRow = position.getRow() + rowDirection;
+                int currentCol = position.getColumn() + colDirection;
+
+                while (currentRow > 0 && currentRow < 9 && currentCol > 0 && currentCol < 9) {
+                    ChessPosition newPosition = new ChessPosition(currentRow, currentCol);
+                    ChessPiece newPositionPiece = board.getPiece(newPosition);
+
+                    if (newPositionPiece == null) {
+                        legalMoves.add(new ChessMove(position, newPosition, null));
+                    } else {
+                        if (newPositionPiece.getTeamColor() != pieceColor) {
+                            legalMoves.add(new ChessMove(position, newPosition, null));
+                        }
+                        break;
+                    }
+                    currentRow += rowDirection;
+                    currentCol += colDirection;
+                }
+            }
+            /*returns all valid move*/
+            return legalMoves;
+        }
+    }
