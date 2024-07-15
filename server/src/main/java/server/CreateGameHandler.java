f06bac7f10ef19b540c6ae4864cdf1fcae5a87f6
created squares for the chess board. implemented getRow and getColumn <lindsayslem@gmail.com>
2024-07-15 15:21:37 -0600

    GameServiceTest createGameFailure fixed

--- server/src/main/java/server/CreateGameHandler.java
+++ server/src/main/java/server/CreateGameHandler.java
@@ -1,6 +1,5 @@
 package server;
 
-import chess.ChessGame;
 import com.google.gson.Gson;
 import spark.Request;
 import spark.Response;
@@ -8,6 +7,7 @@ import spark.Route;
 import service.GameService;
 import model.GameData;
 import dataaccess.DataAccessException;
+import model.Error;
 
 public class CreateGameHandler implements Route {
     private final GameService gameService;
@@ -20,30 +20,17 @@ public class CreateGameHandler implements Route {
     @Override
     public Object handle(Request request, Response response) {
         try {
+            String authToken = request.headers("Authorization").replace("Bearer ", "");
             GameData gameData = gson.fromJson(request.body(), GameData.class);
-            if (gameData == null || gameData.getGameName() == null) {
-                response.status(400);
-                return gson.toJson(new GameData(gameData.getGameID(), gameData.getWhiteUsername(), gameData.getBlackUsername(), gameData.getGameName(), new ChessGame()));
-            }
-
-            String authToken = request.headers("Authorization");
-            if (authToken != null && authToken.startsWith("Bearer ")) {
-                authToken = authToken.substring(7);
-            } else {
-                response.status(401);
-                return gson.toJson(new GameData(0, null, null, "Error: unauthorized", null));
-            }
-
-            GameData createdGame = gameService.createGame(gameData.getGameName(), authToken);
+            GameData createdGame = gameService.createGame(gameData, authToken);
             response.status(200);
             return gson.toJson(createdGame);
         } catch (DataAccessException e) {
             response.status(401);
-            GameData gameData = gson.fromJson(request.body(), GameData.class);
-            return gson.toJson(new GameData(gameData.getGameID(), gameData.getWhiteUsername(), gameData.getBlackUsername(), gameData.getGameName(), new ChessGame()));
+            return gson.toJson(new Error("Error: unauthorized"));
         } catch (Exception e) {
             response.status(500);
-            return gson.toJson(new GameData(0, null, null, "Error: internal server error", null));
+            return gson.toJson(new Error("Error: internal server error"));
         }
     }
 }
